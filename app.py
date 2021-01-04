66fe4c6149fec36d23e2ced06081fb38488c39a5
Maarten Meijer <maarten0912@gmail.com>
2021-01-04 16:03:29 +0100

    5 swipes to right

--- app.py
+++ app.py
@@ -2,11 +2,14 @@ import recipesdatabase.dbhelper as db
 import json
 import algorithm
 from flask import Flask, render_template, request
+
 app = Flask(__name__)
+
 
 @app.route('/')
 def main_page():
     return render_template('index.html')
+
 
 @app.route('/swipe', methods=['POST'])
 def swipe_page():
@@ -14,8 +17,8 @@ def swipe_page():
     swipes = request.form['swipes']
     id_list = ids.split(",")
     swipe_list = swipes.split(",")
-    
-    if len(id_list) >= 5:
+
+    if sum(map(lambda x: x == "yes", swipe_list)) >= 5:
         ingredient_list = []
         for i in range(len(id_list)):
             if swipe_list[i] == "yes":
@@ -36,4 +39,5 @@ def swipe_page():
     next_id = db.get_random_id(done_ids=id_list)
     next_recipe = json.loads(db.get_recipe(next_id))
 
-    return render_template('swipe.html', done=False, next_id=next_id, next_recipe=next_recipe, previous_ids=ids, previous_swipes=swipes)
+    return render_template('swipe.html', done=False, next_id=next_id, next_recipe=next_recipe, previous_ids=ids,
+                           previous_swipes=swipes)
