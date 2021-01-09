dcf48a1b0230ca76363aed9468c0c8bb1dfdd34e
Niek Pennings <h.e.pennings@student.utwente.nl>
2021-01-09 15:23:23 +0100

    goback knop werkt + alles de knoppen van de navbar zien er beter uit

--- app.py
+++ app.py
@@ -20,23 +20,49 @@ def help_page():
 def swipe_page():
     ids = request.form['ids']
     swipes = request.form['swipes']
-    print(ids)
-    print(swipes)
-    id_list = ids.split(",")
-    swipe_list = swipes.split(",")
+    back_num = request.form['back']
+    if back_num == '1':
+        back = True
+    else:
+        back = False
+    if back:
+        id_list = ids.split(",")
+        swipes_list = swipes.split(",")
+        ids = ''
+        swipes = ''
+        for i in range(len(id_list)-1):
+            if i == 0:
+                ids = id_list[i]
+            else:
+                ids += ','+id_list[i]
 
-    # Use this if statement if the required amount of yes swipes needs to be above 5
-    # if sum(map(lambda x: x == "yes", swipe_list)) >= 5:
-    #     recipes = algorithm.getbestrecipes(id_list, swipe_list)
-    #     return render_template('swipe.html', done=True, best_matches=recipes)
+        for j in range(len(swipes_list)-1):
+            if j == 0:
+                swipes = swipes_list[j]
+            else:
+                swipes += ',' + swipes_list[j]
 
-    # Use this if statement if the required amount of swipes need to be above 20
-    if len(swipe_list) == 20:
-        recipes = algorithm.getbestrecipes(id_list, swipe_list)
-        return render_template('swipe.html', done=True, best_matches=recipes)
+        next_id = id_list[len(id_list)-1]
+        next_recipe = json.loads(db.get_recipe(next_id))
 
-    next_id = db.get_random_id(done_ids=id_list)
-    next_recipe = json.loads(db.get_recipe(next_id))
+    else:
+        print(ids)
+        print(swipes)
+        id_list = ids.split(",")
+        swipe_list = swipes.split(",")
+
+        # Use this if statement if the required amount of yes swipes needs to be above 5
+        # if sum(map(lambda x: x == "yes", swipe_list)) >= 5:
+        #     recipes = algorithm.getbestrecipes(id_list, swipe_list)
+        #     return render_template('swipe.html', done=True, best_matches=recipes)
+
+        # Use this if statement if the required amount of swipes need to be above 20
+        if len(swipe_list) == 20:
+            recipes = algorithm.getbestrecipes(id_list, swipe_list)
+            return render_template('swipe.html', done=True, best_matches=recipes)
+
+        next_id = db.get_random_id(done_ids=id_list)
+        next_recipe = json.loads(db.get_recipe(next_id))
 
     return render_template('swipe.html', done=False, next_id=next_id, next_recipe=next_recipe, previous_ids=ids,
-                           previous_swipes=swipes)
+                           previous_swipes=swipes, current_id='')
